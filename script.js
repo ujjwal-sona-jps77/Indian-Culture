a790ebc610e4a7f5e703b586ce7b0bf4d980ed0d
ujjwal-sona-jps77 <ujjwalsonawatjps7@gmail.com>
2024-07-03 13:06:42 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+gsap.from("nav", {
+  y: -100,
+  duration: 1,
+});
+
+gsap.to(".main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: ".main",
+    scroller: "body",
+    start: "top -5%",
+    end: "top -100%",
+    scrub: true,
+  },
+  duration: 1.5,
+});
+
+Shery.mouseFollower({
+  //Parameters are optional.
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+Shery.textAnimate(".page1 .top h1" /* Element to target.*/, {
+  //Parameters are optional.
+  style: 1,
+  y: 10,
+  delay: 0.1,
+  duration: 2,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
+
+
