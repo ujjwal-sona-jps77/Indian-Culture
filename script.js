df37fe671989dfda7a9f84315a7e71e5d63b798e
ujjwal-sona-jps77 <ujjwalsonawatjps7@gmail.com>
2024-07-04 12:56:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,4 +32,27 @@ Shery.textAnimate(".page1 .top h1" /* Element to target.*/, {
   multiplier: 0.1,
 });
 
+Shery.textAnimate("#locate" /* Element to target.*/, {
+  //Parameters are optional.
+  style: 2,
+  y: 10,
+  delay: 0.1,
+  duration: 1.5,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
 
+var loader = document.querySelector("#loader");
+setTimeout(function () {
+  loader.style.top = "-100%";
+}, 4200);
+
+Shery.textAnimate(".box_in h1" /* Element to target.*/, {
+  //Parameters are optional.
+  style: 2,
+  y: 10,
+  delay: 0.1,
+  duration: 1,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
