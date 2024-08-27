1a9361dd4da6998e314faa3cf1249afb5ea73dbd
Ujjwal Sonawat <146330690+ujjwal-sona77@users.noreply.github.com>
2024-08-27 21:07:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,17 +76,3 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
-
-const lenis = new Lenis()
-
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
-
-lenis.on('scroll', ScrollTrigger.update)
-
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 1000)
-})
-
-gsap.ticker.lagSmoothing(0)
