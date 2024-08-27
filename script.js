98df0154aee6b77ece21a5996b9a2b00c1b7bfc5
Ujjwal Sonawat <ujjwalsonawatjps7@gmail.com>
2024-08-27 18:17:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -63,3 +63,17 @@ gsap.from("#about_pp", {
     scrub: 2,
   }
 });
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0)
